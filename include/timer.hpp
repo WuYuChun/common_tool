12fdf0401868eb0d405fcf87a2b8854d53d30e58
messi <xidiantuoersuo@163.com>
2020-06-05 09:53:44 +0800

    [modify]:增加命名空间

--- include/timer.hpp
+++ include/timer.hpp
@@ -1,54 +1,55 @@
 
 #include <chrono>
 
-class TimerBase
-{
-public:
-    virtual void start() {}
-    virtual void stop() {}
-    float microseconds() const noexcept
-    {
-        return mMs * 1000.f;
-    }
-    float milliseconds() const noexcept
-    {
-        return mMs;
-    }
-    float seconds() const noexcept
-    {
-        return mMs / 1000.f;
-    }
-    void reset() noexcept
-    {
-        mMs = 0.f;
-    }
-
-protected:
-    float mMs{0.0f};
-};
+namespace common {
 
 
-template <typename Clock>
-class CpuTimer : public TimerBase
-{
-public:
-    using clock_type = Clock;
+    class TimerBase {
+    public:
+        virtual void start() {}
 
-    void start()
-    {
-        mStart = Clock::now();
-    }
-    void stop()
-    {
-        mStop = Clock::now();
-        mMs += std::chrono::duration<float, std::milli>{mStop - mStart}.count();
-    }
+        virtual void stop() {}
 
-private:
-    std::chrono::time_point<Clock> mStart, mStop;
-}; // class CpuTimer
+        float microseconds() const noexcept {
+            return mMs * 1000.f;
+        }
 
-using PreciseCpuTimer = CpuTimer<std::chrono::high_resolution_clock>;
+        float milliseconds() const noexcept {
+            return mMs;
+        }
+
+        float seconds() const noexcept {
+            return mMs / 1000.f;
+        }
+
+        void reset() noexcept {
+            mMs = 0.f;
+        }
+
+    protected:
+        float mMs{0.0f};
+    };
 
 
+    template<typename Clock>
+    class CpuTimer : public TimerBase {
+    public:
+        using clock_type = Clock;
+
+        void start() {
+            mStart = Clock::now();
+        }
+
+        void stop() {
+            mStop = Clock::now();
+            mMs += std::chrono::duration<float, std::milli>{mStop - mStart}.count();
+        }
+
+    private:
+        std::chrono::time_point<Clock> mStart, mStop;
+    }; // class CpuTimer
+
+    using PreciseCpuTimer = CpuTimer<std::chrono::high_resolution_clock>;
+
+}
 int test(char *name);
